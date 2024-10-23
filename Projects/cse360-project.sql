7e6bc1c2cda82264240ae7200b63fb188a385dc8
Muhammed Topiwala <muhammedhunaid@gmail.com>
2024-10-22 22:08:16 -0700

    Clear: Repo

--- Projects/cse360-project.sql
+++
@@ -1,1 +0,0 @@
-SELECT username, first_name, actual_role_column_name FROM users
--- Projects/cse360project/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module asu.cse360project {
-    requires transitive javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-    opens asu.cse360project to javafx.fxml;
-    opens asu.cse360project.DashboardControllers to javafx.fxml;
-    exports asu.cse360project;
-}
