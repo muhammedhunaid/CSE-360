d9f8487c8926350d0fed83afe6143d177a45100c
HunterFlechsig <hunterflechsig@gmail.com>
2024-11-18 20:49:49 -0700

    junit added

---
+++ cse360project/src/main/java/asu/cse360project/Testing/TestScriptArticlesTest.java
@@ -0,0 +1,17 @@
+package asu.cse360project.Testing;
+import org.junit.Test;
+import org.junit.Before;    
+    
+public class TestScriptArticlesTest {
+
+    @Before
+    public void setup(){
+
+    }
+        
+    @Test
+    public void test() {
+        
+    }
+}
+    
